064972f52e2d9bdaa4bb9885663948c879dcb05c
JL-GH <jlingh1993@gmail.com>
2019-10-02 18:56:10 -0400

    adding stretch

--- questions/04_zip/zip.js
+++ questions/04_zip/zip.js
@@ -2,11 +2,20 @@
 write the zip function
 zip([{x: 1}, {x: 2}]) => {x: 3}
 zip([
-  {x: 1, y: 2}, 
+  {x: 1, y: 2},
   {x: 2, z: 1},
   {z: 2, y: 1}]) => {x: 3, y: 3, z: 3}
 */
 
-function zip(objs) {}
+function zip(objs) {
+  let keys = Object.keys(objs)
+  let newObj = {}
+
+  keys.forEach((elem) => {
+    newObj = obj[elem]: 0
+  })
+
+  for (let key in newObj)
+}
 
 module.exports = { zip };
