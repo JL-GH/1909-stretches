3c1daa0ca98c73a9eca31d1daa2b1d591dc09ff5
JL-GH <jlingh1993@gmail.com>
2019-10-16 18:56:35 -0400

    upload stretch

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,3 +1,19 @@
-function groupBy(arr, attr) {}
+function groupBy(arr, attr) {
+  let groupObj = {}
+
+  if (typeof attr === 'string') {
+    arr.forEach((elem) => {
+      let arr = []
+      groupObj[elem.attr] = [elem]
+    })
+  }
+  else if (typeof attr === 'function') {
+    arr.forEach((elem) => {
+      groupObj[attr()] = attr(elem)
+    })
+  }
+
+  return groupObj
+}
 
 module.exports = { groupBy };
