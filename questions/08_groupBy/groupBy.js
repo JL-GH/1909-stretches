58b06a0759ef571613439de4aa175862dfe4f88d
JL-GH <jlingh1993@gmail.com>
2019-10-16 19:10:40 -0400

    upload stretch

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,19 +1,33 @@
 function groupBy(arr, attr) {
-  let groupObj = {}
+  // let groupObj = {}
 
-  if (typeof attr === 'string') {
-    arr.forEach((elem) => {
-      let arr = []
-      groupObj[elem.attr] = [elem]
-    })
-  }
-  else if (typeof attr === 'function') {
-    arr.forEach((elem) => {
-      groupObj[attr()] = attr(elem)
-    })
-  }
+  // if (typeof attr === 'string') {
+  //   arr.forEach((elem) => {
+  //     let arr = []
+  //     groupObj[elem.attr] = [elem]
+  //   })
+  // }
+  // else if (typeof attr === 'function') {
+  //   arr.forEach((elem) => {
+  //     groupObj[attr()] = attr(elem)
+  //   })
+  // }
 
-  return groupObj
+  // return groupObj
+
+  //class review:
+  return arr.reduce((accumulatedDict, nextItem) => {
+    let accumulatedDictKey;
+
+    if (typeof attr === 'function') accumulatedDictKey = attr(nextItem)
+    else accumulatedDictKey = nextItem[attr]
+
+    accumulatedDict[accumulatedDictKey] = !accumulatedDict[accumulatedDictKey]
+      ? [nextItem]
+      : accumulatedDict[accumulatedDictKey].concat([nextItem])
+
+    return accumulatedDict
+  }, {})
 }
 
 module.exports = { groupBy };
