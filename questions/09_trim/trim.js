f4c720781d799b737843c7df09e218d2506cb4e3
JL-GH <jlingh1993@gmail.com>
2019-10-21 19:01:30 -0400

    uploading

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -2,7 +2,7 @@ const trim = obj => {
   let newObj = {}
 
   for (let key in obj) {
-    if (!(obj[key] === undefined) || !(obj[key] === null)) {
+    if (!(obj[key] === undefined) && !(obj[key] === null)) {
       newObj[key] = obj[key]
     }
   }
